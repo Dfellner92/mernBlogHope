425d56e64d7b100b071d4f8ba00eca48170a4a99
Dan Fellner <dfellner92@gmail.com>
2021-09-23 22:19:09 -0400

    checking src

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
@@ -10,13 +10,24 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // mongoose
-mongoose.connect("mongodb+srv://Dfellner92:!Mega24fan@cluster0.zsg4k.mongodb.net/mernBlog?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://Dfellner92:!Mega24fan@cluster0.zsg4k.mongodb.net/mernBlog?retryWrites=true&w=majority"
+);
+
+// data schema
+const movieSchema = {
+  title: String,
+  genre: String,
+  year: String,
+};
+
+const Movie = mongoose.model("Movie", movieSchema);
 
 // API routes
-app.get('/', ((req, res) => {
-    res.send("express is here");
-}));
+app.get("/movies", (req, res) => {
+  Movie.find().then((movies) => res.json(movies));
+});
 
-app.listen(port, function() {
-    console.log("express is running");
-})
+app.listen(port, function () {
+  console.log("express is running");
+});
